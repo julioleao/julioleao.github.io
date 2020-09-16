3a84a1a1e591a2b961ae1f7e7c431cad0b2cffab
julioleao <julio0023@live.com>
2020-09-16 17:25:06 -0300

    Updates

---
+++ precache-manifest.30d1c0bfdd6b917721e039f4c7346be6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be63f87f3826f26b9111c8fa60dc707",
+    "url": "/web2-project2/index.html"
+  },
+  {
+    "revision": "b411beb71bd5adedbcc8",
+    "url": "/web2-project2/static/css/main.305a58b2.chunk.css"
+  },
+  {
+    "revision": "eb9dc8e75d6c2d508073",
+    "url": "/web2-project2/static/js/2.f02dfae1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web2-project2/static/js/2.f02dfae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b411beb71bd5adedbcc8",
+    "url": "/web2-project2/static/js/main.6c6d770f.chunk.js"
+  },
+  {
+    "revision": "e7209b8f7a8bb85aa17d",
+    "url": "/web2-project2/static/js/runtime-main.070f3504.js"
+  },
+  {
+    "revision": "0bcd48e4cd7e1f2200a6e6e1b81fa720",
+    "url": "/web2-project2/static/media/logo.0bcd48e4.png"
+  }
+]);
